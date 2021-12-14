16ce9356c05d92ffaa02b56d6fc8a6f1b971a864
Laura Minter <lbodine@gmail.com>
2021-12-14 11:24:38 -0800

    working on dropdown menu

---
+++ app3.py
@@ -0,0 +1,95 @@
+import dash
+from dash import dcc
+from dash import html
+import plotly.express as px
+import pandas as pd
+
+app = dash.Dash(__name__)
+
+colors = {
+    'background': 'white',
+    'text': 'mediumturquoise',
+    'titletext':'mediumslateblue'
+}
+
+df = pd.read_csv(
+    "./data/seattle_ucv_kids_cleaned.csv")
+df
+
+print(df.head())
+fig = px.scatter(
+    df,
+    x="density",
+    y="frac_change",
+    size="2020",
+    #color="continent",
+    hover_name="neighborhood",
+    log_x=False,
+    size_max=60,
+    labels = {'frac_change':'fractional change in population', 'density':'density'}
+)
+
+fig2 = px.scatter(
+    df,
+    x="area",
+    y="frac_change",
+    size="2020",
+    #color="continent",
+    hover_name="neighborhood",
+    log_x=False,
+    size_max=60,
+    labels = {'frac_change':'fractional change in population', 'area':'area'}
+)
+
+
+
+
+app.layout = html.Div([
+    html.Div([
+        html.Label(['Choose parameter']),
+        dcc.Dropdown(
+            id='parameter_dropdown',
+            options=[
+                     {'label': 'density', 'value': 'density'},
+                     {'label': 'area', 'value': 'area'}
+            ],
+            value='density',
+            multi=False,
+            clearable=False,
+            style={"width": "50%"}
+        ),
+    ]),
+
+    html.Div([
+        dcc.Graph(id='main_graph',figure = fig2)
+    ]),
+
+])
+
+@app.callback(
+    Output(component_id='main_graph', component_property='figure'),
+    [Input(component_id='my_dropdown', component_property='value')]
+)
+
+
+def update_graph(parameter_dropdown):
+    dff = df
+
+
+    chart = px.scatter(
+        df,
+        x=parameter_dropdown,
+        y="frac_change",
+        size="2020",
+        #color="continent",
+        hover_name="neighborhood",
+        log_x=False,
+        size_max=60,
+        labels = {'frac_change':'fractional change in population', 'density':'density'}
+    )
+
+    return (chart)
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
